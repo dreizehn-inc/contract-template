d0d12edd021b8f4eca303e12776e2472973cb3dc
k3nt0w <kento.watanabe.11@gmail.com>
2022-09-01 16:11:17 +0900

    feat: greeter

--- packages/contract/src/greeter.ts
+++ packages/contract/src/greeter.ts
@@ -1,14 +1,9 @@
 import GreeterArtifact from '../artifacts/contracts/Greeter.sol/Greeter.json'
 import { ethers } from 'ethers'
 
-const provider = new ethers.providers.JsonRpcProvider()
-const signer = provider.getSigner(0)
-
-export const Greeter = (contractAddress: string) => {
-  const contract = new ethers.Contract(contractAddress, GreeterArtifact.abi, provider)
+export const Greeter = (contractAddress: string, singer: ethers.providers.JsonRpcSigner) => {
+  const contract = new ethers.Contract(contractAddress, GreeterArtifact.abi, singer)
   return {
-    provider,
-    signer,
     contract
   }
 }
