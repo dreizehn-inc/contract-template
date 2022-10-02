09f2480f966c0b875c2894f4afa00c7d86de7cb3
Kento Watanabe <kento.watanabe.11@gmail.com>
2022-10-02 12:36:04 +0900

    feat: nft (#4)

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv'
 
 import { HardhatUserConfig, task } from 'hardhat/config'
+import '@openzeppelin/hardhat-upgrades'
 import '@typechain/hardhat'
 import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
@@ -16,6 +17,7 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
+    // eslint-disable-next-line no-console
     console.log(account.address)
   }
 })
